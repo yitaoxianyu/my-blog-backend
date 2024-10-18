04ef17ac37812a508f3de5c15516e0940858cd17
XianYu <2861587619@qq.com>
2024-10-18 22:23:23 +0800

    init

---
+++ src/main/java/com/xianyu/myblogbackend/MyBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xianyu.myblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogBackendApplication.class, args);
+    }
+
+}
